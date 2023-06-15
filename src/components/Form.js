ae4b59c66cf8b6c092f3ab33300df0c056cc846b
ChanRithy27 <chanrithy27@icloud.com>
2023-06-14 17:47:30 -0700

    First commit in creating a form application.

---
+++ src/components/Form.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const Form = (props) => {
+    const [ firstName, setFirstName ] = useState("");
+    const [ lastName, setLastName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ cPassword, setCPassword ] = useState("");
+    return (
+        <div>
+            <form>
+                <div>
+                    <h1>
+                        Create Account
+                    </h1>
+                </div>
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" name="firstName" onChange = { (event) => setFirstName(event.target.value) }/>
+                </div>
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" name="lastName" onChange = { (event) => setLastName(event.target.value) }/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" onChange = { (event) => setEmail(event.target.value) }/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" onChange = { (event) => setPassword(event.target.value) }/>
+                </div>
+                <div>
+                    <label htmlFor="cPassword">Confirm Password</label>
+                    <input type="password" name="cPassword" onChange = { (event) => setCPassword(event.target.value) }/>
+                </div>
+            </form>
+            <div>
+                <h1>
+                    Your Account Data
+                </h1>
+                <p>
+                    <em>First Name:</em>{firstName}
+                </p>
+                <p>
+                    <em>Last Name:</em>{lastName}
+                </p>
+                <p>
+                    <em>Email:</em>{email}
+                </p>
+                <p>
+                    <em>First Name:</em>{firstName}
+                </p>
+                <p>
+                    <em>First Name:</em>{firstName}
+                </p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Form;
